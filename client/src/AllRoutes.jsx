b3b2019b141e085693cc204f840038ffa82ca59c
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2023-11-07 18:54:44 +0530

    added dashboard section

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import LandingPage from './Components/Admin/LandingPage/LandingPage';
 import Navbr from './Components/Admin/Navbar/Navbar';
 import Auth from './Components/Admin/Auth/Auth';
+import Dashboard from './Components/Admin/Dashboard/Dashboard';
 
 
 export default function AllRoutes() {
+
+  
+
   return (
     <>
     <Navbr/>
     <Routes>
       <Route path="/" element={<LandingPage/>}></Route>
       <Route path="/auth" element={<Auth/>}></Route>
+      <Route path="/dashboard" element={<Dashboard/>}></Route>
     </Routes>
   </>
   )
